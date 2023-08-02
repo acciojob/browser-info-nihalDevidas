5afabee0235a8f3f1bcc75e8903413aa6fad79ac
nihalDevidas <117895185+nihalDevidas@users.noreply.github.com>
2023-08-02 22:07:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+const divEle = document.getElementById("browser-info");
+
+const str = `You are using ${navigator.appName} version ${navigator.appVersion}`;
+divEle.innerText = str;
